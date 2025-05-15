59dfb52c52b7badf65086b459cec0adec915b5ee
syedmuhdhaikal <syedmuhdhaikal@CSL-0527>
2025-05-15 11:23:23 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Windows.Controls;
+using System.Windows;
+using PrismDemo.Views;
+using Prism.Regions;
+using PrismDemo.Core.Regions;
+using Prism.Modularity;
+using ModuleA;
+
+// Properly add namespaces required
+
+namespace PrismDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            regionAdapterMappings.RegisterMapping(typeof(StackPanel),
+                Container.Resolve<StackPanelRegionAdapter>());
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule<ModuleAModule>();
+        }
+    }
+}
---
+++ Core/Regions/StackPanelRegionAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Regions;
+using System.Windows.Controls;
+
+namespace PrismDemo.Core.Regions
+{
+    public class StackPanelRegionAdapter : RegionAdapterBase<StackPanel>
+    {
+        public StackPanelRegionAdapter(RegionBehaviorFactory behaviorFactory)
+            : base (behaviorFactory)
+        {
+
+        }
+
+        protected override void Adapt(IRegion region, StackPanel regionTarget)
+        // Logic that takes incoming view and add it to Stack Panel
+        {
+            
+            region.Views.CollectionChanged += (s, e) => // Access the view and hook to CollectionChanged event
+            {
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    foreach (var item in e.NewItems)
+                    {
+                        if (item is System.Windows.UIElement element) // Checks if the item is a UIElement first before adding
+                        {
+                            regionTarget.Children.Add(element);
+                        }
+                    }
+                }
+                else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+                {
+                    foreach (var item in e.OldItems)
+                   
+                    {
+                        if (item is System.Windows.UIElement element) // Checks if the item is a UIElement first before removing (Pattern Matching)
+                        {
+                            regionTarget.Children.Remove(element); // If dont check then it will throw an exception because regionTarget.Children only accept UIElement objects
+                        }
+                    }
+                }
+            };
+        }
+        protected override IRegion CreateRegion()
+        {
+            return new Region();
+        }
+    }
+}
